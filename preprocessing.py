8fe24b810d6da2be57c0446d5edc6157bf063e5a
nid2S <s20034@gsm.hs.kr>
2021-12-14 11:35:18 +0900

    MAKING DATASET

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,5 @@
 from transformers import GPT2TokenizerFast, BertTokenizerFast
-from typing import Sequence
+from typing import Sequence, Union
 import tensorflow as tf
 import pandas as pd
 import random
@@ -7,7 +7,54 @@ import json
 import os
 
 def make_RowDataset():
-    train = pd.DataFrame()
+    columns = ["S1", "R1", "S2", "R2", "S3", "R3"]
+
+    train_file_num = 1
+    val_file_num = 1
+    train = pd.DataFrame(columns=columns)
+    val = pd.DataFrame(columns=columns)
+
+    def check_dataset(order: str):
+        assert order == "train" or order == "val"
+        if order == "train":
+            nonlocal train
+            if train.iloc[-1].name >= 20000:
+                nonlocal train_file_num
+                train.to_csv(f"data/combined_raw_dataset/{order}/raw_data{train_file_num}.tsv", sep="\t", encoding="utf-8", index=False, na_rep="")
+                print(f"Train file raw_data{train_file_num} was saved.")
+                train_file_num += 1
+                train = pd.DataFrame(columns=columns)
+        else:
+            nonlocal val
+            if val.iloc[-1].name >= 20000:
+                nonlocal val_file_num
+                val.to_csv(f"data/combined_raw_dataset/{order}/raw_data{val_file_num}.tsv", sep="\t", encoding="utf-8", index=False, na_rep="")
+                print(f"Validation file raw_data{val_file_num} was saved.")
+                val_file_num += 1
+                val = pd.DataFrame(columns=columns)
+
+    print("Data making start.")
+    # ChatbotData
+    chatbot_data = pd.read_csv("data/raw_dataset/Chatbot/ChatbotData.csv", names=["S1", "R1", "labels"]).loc[1:].drop(["labels"], axis=1)
+    train = train.append(chatbot_data, ignore_index=True)
+    print("Chatbot data ended.")
+
+    # emotional
+    for emotional_file_name in os.listdir("data/raw_dataset/감성대화"):
+        for conv in json.load(open("data/raw_dataset/감성대화/"+emotional_file_name, "r+", encoding="utf-8")):
+            temp = []
+            for sent in conv["talk"]["content"].values():
+                temp.append(sent)
+
+            if "Training" in emotional_file_name:
+                train.append(pd.DataFrame([temp], columns=["S1", "R1", "S2", "R2", "S3", "R3"]), ignore_index=True)
+                check_dataset("train")
+            else:
+                val.append(pd.DataFrame([temp], columns=["S1", "R1", "S2", "R2", "S3", "R3"]), ignore_index=True)
+                check_dataset("val")
+    print("EmotionalData ended.")
+
+
 
 def make_Dataset():
     pass
