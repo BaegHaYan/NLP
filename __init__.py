24fc1e1c8ef76ed0e8bc50d993acd483a8835a44
nid2S <s20034@gsm.hs.kr>
2021-12-28 12:10:57 +0900

    change file structure
    Former-commit-id: eb72a55e0ca831349a0e68487ca72eb858fffe51

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-from transformers import TFAutoModelForCausalLM, TFBertModel
-from preprocessing import Preprocesser
+from transformers import TFBertModel
+from HaYan_NLP.model_making.preprocessing import Preprocesser
 
 # preprocesser
 p = Preprocesser()
 # model
 chat_model = None
-compress_model = TFBertModel.from_pretrained(p.COMPRESS_MODEL_NAME, from_pt=True, use_cache=True,
-                                             cache_dir="./model/compress")
+# compress_model = TFBertModel.from_pretrained(p.COMPRESS_MODEL_NAME, from_pt=True, use_cache=True,
+#                                              cache_dir="./model/compress")
