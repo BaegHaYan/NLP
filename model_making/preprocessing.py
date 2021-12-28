e7d4e24ebf4f432825d46345205d98ac4aaa42fb
nid2S <s20034@gsm.hs.kr>
2021-12-28 17:21:27 +0900

    torch start, torch preprocessing
    Former-commit-id: 91182c06ec558ed4ba92f7e4e8ca4978cbb3eae1

--- model_making/preprocessing.py
+++ model_making/preprocessing.py
@@ -1,7 +1,9 @@
 from transformers import GPT2TokenizerFast, BertTokenizerFast
+from torch.utils.data import TensorDataset, DataLoader
 from typing import Sequence
 import tensorflow as tf
 import pandas as pd
+import torch
 import random
 import json
 import os
@@ -215,8 +217,6 @@ class Preprocesser:
         self.max_len = 201  # train_x : 184 | train_Y : 201 | val_x : 127 | val_Y : 148
         # data
         self.data_num = 3798  # train - 3362, val - 436
-        # self.PREMODEL_NAME = "kakaobrain/kogpt"
-        # self.REVISION_NAME ='KoGPT6B-ryan1.5b-float16'
         self.PREMODEL_NAME = "byeongal/Ko-DialoGPT"
         self.COMPRESS_MODEL_NAME = "monologg/kobert"
         self.tokenizer = GPT2TokenizerFast.from_pretrained("../tokenizer")
@@ -251,8 +251,37 @@ class Preprocesser:
                                                  max_length=self.max_len, padding="max_length", truncation=True)["input_ids"]
         return tf.data.Dataset.from_tensor_slices((encoded_val_x, val_Y)).batch(self.batch_size).shuffle(256, seed=self.RANDOM_SEED)
 
+    def getTorchTrainData(self) -> DataLoader:
+        torch.manual_seed(self.RANDOM_SEED)
+        torch.cuda.manual_seed(self.RANDOM_SEED)
+
+        train_data = pd.read_csv("../data/train.txt", sep="\t", names=["dialogue", "response"], header=0)
+
+        train_x = self.tokenizer.batch_encode_plus(train_data["dialogue"].to_list(), return_tensors="pt",
+                                                   max_length=self.max_len, padding="max_length", truncation=True)
+        train_Y = self.tokenizer.batch_encode_plus((train_data["dialogue"] + train_data["response"]).to_list(),
+                                                   return_tensors="pt", max_length=self.max_len, padding="max_length", truncation=True)["input_ids"]
+        train_dataset = TensorDataset(train_x["input_ids"], train_x["attention_mask"], train_Y)
+
+        return DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+
+    def getTorchValidationData(self) -> DataLoader:
+        torch.manual_seed(self.RANDOM_SEED)
+        torch.cuda.manual_seed(self.RANDOM_SEED)
+
+        val_data = pd.read_csv("../data/val.txt", sep="\t", names=["dialogue", "response"], header=0)
+
+        val_x = self.tokenizer.batch_encode_plus(val_data["dialogue"].to_list(), return_tensors="pt",
+                                                 max_length=self.max_len, padding="max_length", truncation=True)
+        val_Y = self.tokenizer.batch_encode_plus((val_data["dialogue"] + val_data["response"]).to_list(),
+                                                 return_tensors="pt", max_length=self.max_len, padding="max_length", truncation=True)["input_ids"]
+        val_dataset = TensorDataset(val_x["input_ids"], val_x["attention_mask"], val_Y)
+
+        return DataLoader(val_dataset, batch_size=self.batch_size, shuffle=True)
+
     def encoding(self, text: str) -> tf.Tensor:
-        return self.tokenizer.encode(text, return_tensors="tf")
+        # return self.tokenizer.encode(text, return_tensors="tf")
+        return self.tokenizer.encode(text, return_tensors="pt")
 
     def decoding(self, ids: Sequence[int]) -> str:
         return self.tokenizer.decode(ids, skip_special_tokens=True)
