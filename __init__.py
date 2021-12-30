d8515f7d1b8c5a4a326fcb4170bba313724dddc0
nid2S <s20034@gsm.hs.kr>
2021-12-30 18:06:41 +0900

    temp processing
    Former-commit-id: c17c49f6bfa0da8646b2245f8e8f1904f0cd7463

--- __init__.py
+++ __init__.py
@@ -6,5 +6,6 @@ import torch
 p = Preprocesser()
 # model
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-chat_model = GPT2LMHeadModel.from_pretrained("./model/hf_form").to(device)
+chat_model = GPT2LMHeadModel.from_pretrained("byeongal/Ko-DialoGPT").to(device)
+# chat_model = GPT2LMHeadModel.from_pretrained("./model/hf_form").to(device)
 # compress_model = TFBertModel.from_pretrained(p.COMPRESS_MODEL_NAME, from_pt=True, use_cache=True, cache_dir="./model/compress")
