7c49c49e5faac3b840fdb668da51869e6aad90fa
nid2S <s20034@gsm.hs.kr>
2021-12-30 15:32:17 +0900

    using model
    Former-commit-id: c6e0a2688f9aa02acc6807a1167a87914f259f90

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
-from transformers import TFBertModel
 from HaYan_NLP.model_making.preprocessing import Preprocesser
+from transformers import GPT2LMHeadModel
+import torch
 
 # preprocesser
 p = Preprocesser()
 # model
-chat_model = None
-# compress_model = TFBertModel.from_pretrained(p.COMPRESS_MODEL_NAME, from_pt=True, use_cache=True,
-#                                              cache_dir="./model/compress")
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+chat_model = GPT2LMHeadModel.from_pretrained("./model/hf_form").to(device)
+# compress_model = TFBertModel.from_pretrained(p.COMPRESS_MODEL_NAME, from_pt=True, use_cache=True, cache_dir="./model/compress")
--- model_use.py
+++ model_use.py
@@ -1,21 +1,27 @@
-import __init__
-import numpy as np
 from typing import List
+import __init__
+import torch
 
-def use_chat_model(text: str) -> str:
-    text = __init__.p.encoding(text)
-    output = __init__.chat_model.generate(text,
+using_turn = 2
+max_length = 1000
+eos_token = __init__.p.tokenizer.eos_token
+def use_chat_model(user_text: str, past_convs: List[str]) -> str:
+    user_text = __init__.p.encoding(user_text+eos_token)
+    for i in range(len(past_convs)-1, len(past_convs)-(1+using_turn*2), -1):
+        if i < 0:
+            break
+        encoded_vector = __init__.p.encoding(past_convs[i]+eos_token)
+        if user_text.shape[-1] + encoded_vector.shape[-1] < max_length:
+            user_text = torch.cat([encoded_vector, user_text], dim=-1)
+        else:
+            break
+
+    user_text = user_text.to(__init__.device)
+    output = __init__.chat_model.generate(user_text,
                                           max_length=1000,
                                           num_beams=5,
                                           top_k=20,
                                           no_repeat_ngram_size=4,
                                           length_penalty=0.65,
                                           repetition_penalty=2.)
-    return __init__.p.decoding(output[0][text.shape[-1]:])
-
-def compress_sent(sent: str) -> List[float]:
-    encoded_sent = __init__.p.compress_tokenizer.encode(sent, return_tensors="tf")
-    return __init__.compress_model(encoded_sent)["pooler_output"].numpy().tolist()
-
-def cos_sim(v1: List[float], v2: List[float]) -> np.float:
-    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+    return __init__.p.decoding(output[0][user_text.shape[-1]:])
