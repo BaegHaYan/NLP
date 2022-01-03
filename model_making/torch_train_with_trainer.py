90b328acef1a7cd7b745e21cd82a6712a56adbdd
nid2S <s20034@gsm.hs.kr>
2022-01-03 11:50:22 +0900

    trian with trainer code
    Former-commit-id: 52cd58246e0535a59971b6b6eaba96dc08269654

--- model_making/torch_train_with_trainer.py
+++ model_making/torch_train_with_trainer.py
@@ -1,31 +1,56 @@
-from transformers import GPT2LMHeadModel, Trainer, TrainingArguments
-from preprocessing import Preprocesser
+from transformers import Trainer, TrainingArguments, DataCollatorWithPadding
+from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+import pandas as pd
 import datetime
 import torch
 import os
 
 if __name__ == "__main__":
-    class koDialoGPT(torch.nn.Module):
-        def __init__(self):
-            super(koDialoGPT, self).__init__()
-            self.model = GPT2LMHeadModel.from_pretrained(p.PREMODEL_NAME)
+    PREMODEL_NAME = "byeongal/Ko-DialoGPT"
+    RANDOM_SEED = 10
+    MAX_LEN = 201
 
-        def forward(self, x):
-            input_ids, attention_mask = x
-            logits = self.model(input_ids=input_ids, attention_mask=attention_mask, return_dict=True)
-            return logits.logits
+    def getDataset(isTrain: bool, using_device: str):
+        torch.manual_seed(RANDOM_SEED)
+        torch.cuda.manual_seed(RANDOM_SEED)
 
-    def compute_metric():
-        pass
+        if isTrain:
+            data_path = "../data/train.txt"
+        else:
+            data_path = "../data/val.txt"
 
-    p = Preprocesser()
+        encoded_data = []
+        data = pd.read_csv(data_path, sep="\t", names=["dialogue", "response"], header=0)
+        for d, r in zip(data["dialogue"], data["response"]):
+            temp_element = dict()
+
+            d_tok = tokenizer(d, return_tensors="pt", max_length=MAX_LEN, padding="max_length", truncation=True)
+            for k, v in d_tok.items():
+                temp_element[k] = v.to(using_device)
+            temp_element["labels"] = tokenizer(d+r, return_tensors="pt", max_length=MAX_LEN,
+                                               padding="max_length", truncation=True)["input_ids"].to(using_device)
+
+            encoded_data.append(temp_element)
+        return encoded_data
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = koDialoGPT().to(device)
     epochs = 5
+    batch_size = 16
 
-    log_dir = os.path.join('./logs', datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
-    train_args = TrainingArguments(output_dir="../model/torch_models/traniner_model", logging_dir=log_dir, num_train_epochs=epochs)
+    model = GPT2LMHeadModel.from_pretrained(PREMODEL_NAME).to(device)
+    tokenizer = GPT2TokenizerFast.from_pretrained("../tokenizer")
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-    trainer = Trainer(model=model, args=train_args, compute_metrics=None,
-                      train_dataset=p.getTorchTrainData(), eval_dataset=p.getTorchValidationData())
+    log_dir = os.path.join('../logs', datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
+    train_args = TrainingArguments(output_dir="../model/torch_models/traniner_model",
+                                   logging_dir=log_dir,
+                                   num_train_epochs=epochs,
+                                   per_device_train_batch_size=batch_size,
+                                   per_device_eval_batch_size=batch_size,
+                                   learning_rate=3e-5,
+                                   weight_decay=0.01,
+                                   )
+
+    trainer = Trainer(model=model, args=train_args, tokenizer=tokenizer, data_collator=data_collator,
+                      train_dataset=getDataset(isTrain=True, using_device=device), eval_dataset=getDataset(isTrain=False, using_device=device))
     trainer.train()
