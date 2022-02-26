8dce538076bc92aa35420a845237b248d25fcb2a
nid2S <s20034@gsm.hs.kr>
2022-02-26 23:24:46 +0900

    file rename, dataset combiner

---
+++ dataset_making/combine_raw_dataset.py
@@ -0,0 +1,15 @@
+from transformers import pipeline
+import pandas as pd
+import json
+import os
+
+
+class Dataset_combiner:
+    def __init__(self):
+        pass
+
+    def combine_raw_datasets(self):
+        pass
+
+    def translate(self, text: str, lang: str = None):
+        pass
