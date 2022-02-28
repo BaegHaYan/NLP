6d97daa1e7d6222c964d91642673dc0dfbf85b13
nid2S <s20034@gsm.hs.kr>
2022-02-28 17:48:31 +0900

    label_classification

--- dataset_making/label_classification.py
+++ dataset_making/label_classification.py
@@ -1,5 +1,5 @@
+import pandas as pd
 import setuptools
-import re
 import torch
 import pytorch_lightning as pl
 from torch.utils.data import TensorDataset, DataLoader
@@ -19,16 +19,16 @@ class LabelClassification(LightningModule):
         self.num_labels = 7
         self.learning_rate = 5e-5
         self.batch_size = 32
-        self.input_dim = None
+        self.input_dim = None  # train - 55
         self.pad_token_id = self.tokenizer.pad_token_id
 
         self.MODEL_NAME = "Huffon/klue-roberta-base-nli"
         self.model = BertForSequenceClassification.from_pretrained(self.MODEL_NAME, num_labels=self.num_labels)
         self.tokenizer = BertTokenizerFast.from_pretrained(self.MODEL_NAME)
 
-        self.train_set = None
+        self.label_dict = {'[HAPPY]': 0, '[PANIC]': 1, '[ANGRY]': 2, '[UNSTABLE]': 3, '[HURT]': 4, '[SAD]': 5, '[NEUTRAL]': 6}
+        self.train_set = None  # 3366
         self.val_set = None
-        self.test_set = None
 
     def configure_optimizers(self):
         optim = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
@@ -47,16 +47,44 @@ class LabelClassification(LightningModule):
         return torch.sum(output == labels) / output.__len__() * 100  # %(Precentage)
 
     def prepare_data(self):
-        pass
+        raw_train = pd.read_csv("../data/encoded_dataset/train/train_data1.txt", sep="\t", encoding="949").drop(["R1", "R2", "R3"], axis=1)
+        raw_val = pd.read_csv("../data/encoded_dataset/val/val_data1.txt", sep="\t", encoding="949").drop(["R1", "R2", "R3"], axis=1)
+
+        train_x = []
+        train_Y = []
+        for _, (s1, s2, s3) in raw_train.iterrows():
+            train_x.append(" ".join(s1.split()[1:]))
+            train_Y.append(self.label_dict[s1.split()[0]])
+            if s2 != "NONE":
+                train_x.append(" ".join(s2.split()[1:]))
+                train_Y.append(self.label_dict[s2.split()[0]])
+            if s3 != "NONE":
+                train_x.append(" ".join(s3.split()[1:]))
+                train_Y.append(self.label_dict[s3.split()[0]])
+        train_x = self.tokenizer.batch_encode_plus(train_x, max_length=self.input_dim, padding="max_length", truncation=True, return_tensors="pt")
+        train_Y = torch.LongTensor(train_Y)
+        self.train_set = TensorDataset(train_x["input_ids"], train_Y)
+
+        val_x = []
+        val_Y = []
+        for _, (s1, s2, s3) in raw_val.iterrows():
+            val_x.append(" ".join(s1.split()[1:]))
+            val_Y.append(self.label_dict[s1.split()[0]])
+            if s2 != "NONE":
+                val_x.append(" ".join(s2.split()[1:]))
+                val_Y.append(self.label_dict[s2.split()[0]])
+            if s3 != "NONE":
+                val_x.append(" ".join(s3.split()[1:]))
+                val_Y.append(self.label_dict[s3.split()[0]])
+        val_x = self.tokenizer.batch_encode_plus(val_x, max_length=self.input_dim, padding="max_length", truncation=True, return_tensors="pt")
+        val_Y = torch.LongTensor(val_Y)
+        self.val_set = TensorDataset(val_x["input_ids"], val_Y)
 
     def train_dataloader(self):
         return DataLoader(self.train_set, batch_size=self.batch_size, shuffle=True)
 
     def val_dataloader(self):
         return DataLoader(self.val_set, batch_size=self.batch_size, shuffle=True)
-
-    def test_dataloader(self):
-        return DataLoader(self.test_set, batch_size=self.batch_size, shuffle=True)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -81,18 +109,3 @@ class LabelClassification(LightningModule):
         mean_acc = torch.stack([output['val_acc'] for output in outputs]).mean()
         logs = {'val_loss': mean_loss, 'val_acc': mean_acc}
         return {'avg_val_loss': mean_loss, 'avg_val_acc': mean_acc, 'log': logs}
-
-    def test_step(self, batch, batch_idx):
-        x, y = batch
-        y_pred = self(x)
-        loss = self.cross_entropy_loss(y_pred, y)
-        accuracy = self.accuracy(y_pred, y)
-
-        logs = {'test_loss': loss, 'test_acc': accuracy}
-        return {'loss': loss, 'accuracy': accuracy, 'log': logs}
-
-    def test_epoch_end(self, outputs):
-        mean_loss = torch.stack([output['test_loss'] for output in outputs]).mean()
-        mean_acc = torch.stack([output['test_acc'] for output in outputs]).mean()
-        logs = {'test_loss': mean_loss, 'test_acc': mean_acc}
-        return {'avg_test_loss': mean_loss, 'avg_test_acc': mean_acc, 'log': logs}
