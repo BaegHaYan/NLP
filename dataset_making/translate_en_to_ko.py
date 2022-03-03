17d93c5ded272ea5e16a02faea8fd716fba84147
nid2S <s20034@gsm.hs.kr>
2022-03-03 14:07:12 +0900

    callback, persona changer

--- dataset_making/translate_en_to_ko.py
+++ dataset_making/translate_en_to_ko.py
@@ -9,20 +9,22 @@ import pandas as pd
 import torch
 import os
 import re
+# TODO persoan chager import
 
 class Translater_en_to_ko(pl.LightningModule):
-    def __init__(self, epochs: int, gamma: float = 0.5):
+    def __init__(self):
         super(Translater_en_to_ko, self).__init__()
         self.RANDOM_SEED = 7777
-        torch.manual_seed(self.RANDOM_SEED)
-        torch.cuda.manual_seed(self.RANDOM_SEED)
         pl.seed_everything(self.RANDOM_SEED)
 
         self.model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-cc25")
+        self.persona_changer = None
         self.tokenizer = MBartTokenizerFast.from_pretrained("facebook/mbart-large-cc25")
+        self.pad_token_id = self.tokenizer.pad_token_id
 
-        self.epochs = epochs
-        self.gamma = gamma
+        self.epochs = 10
+        self.gamma = 0.5
+        self.learning_rate = 5e-5
         self.batch_size = 32
         self.max_len_x = 82
         self.max_len_y = 78
@@ -35,12 +37,17 @@ class Translater_en_to_ko(pl.LightningModule):
         lr_scheduler = MultiStepLR(optim, milestones=[int(self.epochs*0.3), int(self.epochs*0.6)], gamma=self.gamma)
         return [optim], [lr_scheduler]
 
+    def configure_callbacks(self):
+        check_point = ModelCheckpoint("../models/translater_en_to_ko/model_ckp/", monitor="val_acc", mode="max")
+        early_stopping = EarlyStopping(monitor="val_loss", mode="min", patience=3)
+        return [check_point, early_stopping]
+
     def forward(self, x):
         output = self.model(x)
-        return output.logit
+        return output.logits
 
     def cross_entropy_loss(self, output, labels):
-        return torch.nn.CrossEntropyLoss()(output, labels, ignore_index=self.pad_token_id)
+        return torch.nn.CrossEntropyLoss(ignore_index=self.pad_token_id)(output, labels)
 
     def prepare_data(self):
         data = pd.DataFrame(columns=['번역문', '원문'])
@@ -52,7 +59,7 @@ class Translater_en_to_ko(pl.LightningModule):
             data.drop([col], axis=1, inplace=True)
         data.columns = ['en', 'ko']
 
-        data['ko'] = data['ko'].apply(lambda x: self.change_korean_honorific(x))
+        data['ko'] = data['ko'].apply(lambda x: self.change_persona(x))
         data_x = self.tokenizer.batch_encode_plus(data['en'].to_list(), max_length=self.max_len_x, padding="max_length", truncation=True, return_tensors="pt")
         data_Y = self.tokenizer.batch_encode_plus(data['ko'].to_list(), max_length=self.max_len_y, padding="max_length", truncation=True, return_tensors="pt")
         train_x, val_x, train_Y, val_Y = train_test_split(data_x['input_ids'], data_Y['input_ids'], train_size=0.7)
@@ -71,43 +78,31 @@ class Translater_en_to_ko(pl.LightningModule):
         y_pred = self(x)
         loss = self.cross_entropy_loss(y_pred, y)
 
-        logs = {'train_loss': loss}
-        return {'loss': loss, 'log': logs}
+        self.log('loss', loss)
+        return {'loss': loss}
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_pred = self(x)
         loss = self.cross_entropy_loss(y_pred, y)
 
-        logs = {'val_loss': loss}
-        return {'loss': loss, 'log': logs}
+        self.log('val_loss', loss)
+        return {'val_loss': loss}
 
     def validation_epoch_end(self, outputs):
         mean_loss = torch.stack([output['val_loss'] for output in outputs]).mean()
-        logs = {'val_loss': mean_loss}
-        return {'avg_val_loss': mean_loss, 'log': logs}
+        self.log('avg_val_loss', mean_loss)
+        return {'avg_val_loss': mean_loss}
 
-    def change_korean_honorific(self, sent) -> str:
-        sent = re.sub("그렇습니다", "그래", sent)
-        sent = re.sub("주세요\.", "줄래?", sent)
-        sent = re.sub("하세요", "해", sent)
-        sent = re.sub("합니다", "해", sent)
-        sent = re.sub("하였다", "했어", sent)
-        sent = re.sub("습니다", "어", sent)
-        sent = re.sub("제가", "내가", sent)
-        sent = re.sub("제게", "내게", sent)
-        sent = re.sub("저희", "우리", sent)
-        sent = re.sub("요", "", sent)
-        sent = re.sub("네([,.?!])", r"응\1", sent)
+    def change_persona(self, sent) -> str:
+        sent = self.persona_changer(sent)
+        # TODO change Tensor to str
         return sent
 
 
-epochs = 5
-model = Translater_en_to_ko(epochs)
-trainer = pl.Trainer(max_epochs=epochs, gpus=torch.cuda.device_count(),
-                     callbacks=[ModelCheckpoint("../models/label_classifier/model_ckp/", verbose=True, monitor="val_acc", mode="max"),
-                                EarlyStopping(monitor="val_loss", mode="min", patience=3)])
+model = Translater_en_to_ko()
+trainer = pl.Trainer(max_epochs=model.epochs, gpus=torch.cuda.device_count())
 
 trainer.fit(model)
-torch.save(model.state_dict(), "../models/label_classifier/torch_model/model_state.pt")
-trainer.save_checkpoint("../models/label_classifier/pl_model/pytorch_model.bin")
+torch.save(model, "../models/translater_en_to_ko/torch_model/pytorch_model.bin")
+trainer.save_checkpoint("../models/translater_en_to_ko/pl_model/pl_model.ptl")
