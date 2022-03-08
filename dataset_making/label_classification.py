f633127a572deb12907a3567eb16bb9b9152d79b
nid2S <s20034@gsm.hs.kr>
2022-03-08 11:14:54 +0900

    label_classification lr change, model change

--- dataset_making/label_classification.py
+++ dataset_making/label_classification.py
@@ -5,12 +5,12 @@ import pandas as pd
 import pytorch_lightning as pl
 import torch.nn.functional as F
 from torch.optim import AdamW
+from torch.optim.lr_scheduler import ExponentialLR
 from torch.utils.data import TensorDataset, DataLoader
 from pytorch_lightning import LightningModule, Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-from transformers.optimization import get_cosine_schedule_with_warmup
-from transformers import BertForSequenceClassification, BertTokenizerFast
+from transformers import ElectraForSequenceClassification, ElectraTokenizerFast
 
 class LabelClassification(LightningModule):
     def __init__(self):
@@ -20,14 +20,15 @@ class LabelClassification(LightningModule):
 
         self.epochs = 10
         self.batch_size = 16
-        self.warmup_ratio = 0.1
+        self.factor = 1e-2
+        self.gamma = 0.9
         self.learning_rate = 5e-3  # 5e-5
         self.num_labels = 7
         self.input_dim = 55  # train - 55, val - 50
 
-        self.MODEL_NAME = "Huffon/klue-roberta-base-nli"
-        self.model = BertForSequenceClassification.from_pretrained(self.MODEL_NAME, num_labels=self.num_labels)
-        self.tokenizer = BertTokenizerFast.from_pretrained(self.MODEL_NAME)
+        self.MODEL_NAME = "monologg/koelectra-base-v3-discriminator"
+        self.model = ElectraForSequenceClassification.from_pretrained(self.MODEL_NAME, num_labels=self.num_labels)
+        self.tokenizer = ElectraTokenizerFast.from_pretrained(self.MODEL_NAME)
         self.pad_token_id = self.tokenizer.pad_token_id
 
         self.label_dict = {'[HAPPY]': 0, '[PANIC]': 1, '[ANGRY]': 2, '[UNSTABLE]': 3, '[HURT]': 4, '[SAD]': 5, '[NEUTRAL]': 6}
@@ -37,16 +38,14 @@ class LabelClassification(LightningModule):
     def configure_optimizers(self):
         optim = AdamW(self.parameters(), lr=self.learning_rate, weight_decay=0.1)
 
-        num_train_steps = len(self.train_dataloader()) * self.epochs
-        num_warmup_steps = int(num_train_steps * self.warmup_ratio)
-        scheduler = get_cosine_schedule_with_warmup(optim, num_warmup_steps=num_warmup_steps, num_training_steps=num_train_steps)
-        lr_scheduler = {'scheduler': scheduler, 'name': 'cosine_schedule_with_warmup', 'monitor': 'loss', 'interval': 'step', 'frequency': 1}
+        scheduler = ExponentialLR(optim, gamma=self.gamma)
+        lr_scheduler = {'scheduler': scheduler, 'name': 'ExponentialLR', 'monitor': 'loss', 'interval': 'step', 'frequency': 1}
         return [optim], [lr_scheduler]
 
     def configure_callbacks(self):
         check_point = ModelCheckpoint(dirpath="../models/label_classifier/model_ckp/", filename='{epoch:02d}_{loss:.2f}',
                                       verbose=True, save_last=True, monitor='loss', mode='min')
-        early_stopping = EarlyStopping(monitor="val_loss", mode="min", patience=4)
+        early_stopping = EarlyStopping(monitor="val_loss", mode="min", patience=5)
         return [check_point, early_stopping]
 
     def forward(self, x):
@@ -131,5 +130,5 @@ model = LabelClassification()
 trainer = Trainer(max_epochs=model.epochs, gpus=torch.cuda.device_count(),
                   logger=TensorBoardLogger("../models/label_classifier/tensorboardLog/"))
 trainer.fit(model)
-torch.save(model, "../models/label_classifier/torch_model/pytorch_model.bin")
 trainer.save_checkpoint("../models/label_classifier/pl_model/pl_model.ptl")
+torch.save(model, "../models/label_classifier/torch_model/pytorch_model.bin")
