ac98be75dcc10fcb9b020eb3a4ad353dafe60f24
nid2S <s20034@gsm.hs.kr>
2022-03-29 08:28:15 +0900

    fixed error(import, schedulers, argments, type, lr)

--- model_making/persona_model/persona_classification.py
+++ model_making/persona_model/persona_classification.py
@@ -1,8 +1,9 @@
+import setuptools
+import torchmetrics
 from torch.utils.data import TensorDataset, DataLoader
 from torch.optim.lr_scheduler import SequentialLR, ConstantLR, ExponentialLR
 from transformers import GPT2TokenizerFast
 import torch
-import torchmetrics
 import argparse
 import logging
 import pandas as pd
@@ -17,19 +18,19 @@ class Persona_classification(torch.nn.Module):
         self.nhead = hparams.nhead
         self.num_layers = hparams.num_layers
         self.dim_feedforward = hparams.dim_feedforward
+        self.vocab_size = 51200
         self.pad_token_id = 3
 
-        self.embed_layer = torch.nn.Embedding(self.input_dim, self.embedding_dim, padding_idx=self.pad_token_id)
+        self.embed_layer = torch.nn.Embedding(self.vocab_size, self.embedding_dim, padding_idx=self.pad_token_id)
         transformer_encoder = torch.nn.TransformerEncoderLayer(self.embedding_dim, self.nhead, dim_feedforward=self.dim_feedforward, batch_first=True)
-        self.transformer_encoder_layer = torch.nn.TransformerEncoder(transformer_encoder, num_layers=self.num_layers, norm=torch.nn.LayerNorm(self.hidden_dim))
+        self.transformer_encoder_layer = torch.nn.TransformerEncoder(transformer_encoder, num_layers=self.num_layers, norm=torch.nn.LayerNorm(self.embedding_dim))
         self.output_layer = torch.nn.Sequential(
+            torch.nn.Linear(self.embedding_dim, self.hidden_dim),
+            torch.nn.ReLU(),
             torch.nn.Linear(self.hidden_dim, int(self.hidden_dim/2)),
             torch.nn.Tanh(),
             torch.nn.LayerNorm(int(self.hidden_dim/2)),
-            torch.nn.Linear(int(self.hidden_dim/2), self.hidden_dim),
-            torch.nn.Tanh(),
-            torch.nn.LayerNorm(self.hidden_dim),
-            torch.nn.Linear(self.hidden_dim, 2)
+            torch.nn.Linear(int(self.hidden_dim/2), 2)
         )
 
     def forward(self, x):
@@ -40,9 +41,8 @@ class Persona_classification(torch.nn.Module):
 
     @staticmethod
     def add_model_argments(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
-        # BERT_base : L=12, H=768, A=12, Total Parameters = 110M, feed-forward/filter size = 4H
-        parser.add_argument("-e", type=int, default=1024, dest="embedding_dim", help="embedding size")
-        parser.add_argument("-h", type=int, default=786, dest="hidden_dim", help="hidden size")
+        parser.add_argument("--embedding-dim", type=int, default=960, dest="embedding_dim", help="embedding size")
+        parser.add_argument("--hidden-size", type=int, default=786, dest="hidden_dim", help="hidden size")
         parser.add_argument("--attention-head", type=int, default=12, dest="nhead", help="num of attention heads")
         parser.add_argument("--num-layers", type=int, default=12, dest="num_layers", help="num of layers")
         parser.add_argument("--dim-feedforward", type=int, default=2048, dest="dim_feedforward", help="dim of feedforward")
@@ -68,7 +68,7 @@ def get_dataset(tokenizer: GPT2TokenizerFast, hparams: argparse.Namespace, logge
             x *= 100
             Y *= 100
         x = torch.LongTensor(x).to(device)
-        Y = torch.LongTensor(Y).to(device)
+        Y = torch.FloatTensor(Y).to(device)
         datasets.append(TensorDataset(x, Y))
     train_dataloader = DataLoader(datasets[0], batch_size=hparams.batch_size, shuffle=True, drop_last=True)
     logger.info("making train dataloader complete")
@@ -104,11 +104,11 @@ if __name__ == "__main__":
     tokenizer = GPT2TokenizerFast.from_pretrained("../../tokenizer/GPT")
     accuracy = torchmetrics.Accuracy()
     loss_func = torch.nn.BCELoss()
-    optim = torch.optim.AdamW(model.parameters())
+    optim = torch.optim.AdamW(model.parameters(), lr=hparams.lr)
 
     constant_scheduler = ConstantLR(optim, factor=hparams.factor)
     exponential_scheduler = ExponentialLR(optim, gamma=hparams.gamma)
-    lr_scheduler = SequentialLR([constant_scheduler, exponential_scheduler], milestones=[int(hparams.epochs * hparams.warmup_rate)])
+    lr_scheduler = SequentialLR(optim, schedulers=[constant_scheduler, exponential_scheduler], milestones=[int(hparams.epochs * hparams.warmup_rate)])
 
     best_metrics = 0
     train_loader, val_loader = get_dataset(tokenizer, hparams, logger, device)
@@ -118,12 +118,15 @@ if __name__ == "__main__":
 
             x, Y = batch
             pred = model(x)
+            pred = pred.view(hparams.batch_size, -1)
+            pred = torch.sum(pred, dim=1)
+            pred = torch.sigmoid(pred)
 
             loss = loss_func(pred, Y)
-            acc = accuracy(pred, Y)
-
-            logger.info(f"Epochs {i+1} : loss - {loss.item()}, acc - {acc.item()} | {j}/{len(train_loader)}({j/len(train_loader) * 100}%)\r")
-
+            acc = accuracy(pred, Y.type(torch.int))
+            if j % 10 == 0:
+                logger.info(f"Epochs {i+1} : loss - %.4f, acc - %.4f | {j+1}/{len(train_loader)}(%.2f%%)"
+                            % (loss.item(), acc.item()*100, (j+1)/len(train_loader) * 100))
             loss.backward()
             optim.step()
 
@@ -133,11 +136,14 @@ if __name__ == "__main__":
             for batch in val_loader:
                 x, Y = batch
                 pred = model(x)
+                pred = torch.sum(pred, dim=1)
+                pred = torch.sigmoid(pred)
+
                 val_loss.append(loss_func(pred, Y).item())
-                val_acc.append(accuracy(pred, Y).item())
+                val_acc.append(accuracy(pred, Y.type(torch.int)).item())
             val_loss = torch.mean(torch.tensor(val_loss))
             val_acc = torch.mean(torch.tensor(val_acc))
-            logger.info(f"Epochs {i+1} : val_loss - {val_loss.item()}, val_acc - {val_acc.item()})")
+            logger.info(f"Epochs {i+1} : val_loss - %.4f, val_acc - %.4f)" % (val_loss.item(), val_acc.item()))
 
             if val_loss > best_metrics:
                 logger.info(f"val_loss has achieved the best : {best_metrics} to {val_loss}")
@@ -147,3 +153,5 @@ if __name__ == "__main__":
                 logger.info("the model state was saved.")
             else:
                 logger.info(f"the model state was NOT achieved the best. | the best - {best_metrics}")
+
+        lr_scheduler.step()
