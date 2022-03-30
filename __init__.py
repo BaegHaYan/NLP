3421b98d91a13343371cf6ad1d3fe840d35f7c98
nid2S <s20034@gsm.hs.kr>
2022-03-30 15:02:06 +0900

    model using code

--- __init__.py
+++ __init__.py
@@ -1,10 +1,27 @@
-from HaYan_NLP.preprocessing import Preprocesser
-from transformers import GPT2LMHeadModel, BertModel
+import setuptools
+from transformers import GPT2LMHeadModel, BartForConditionalGeneration, PreTrainedTokenizerFast
+from HaYan_NLP.model_making.persona_model.persona_classification import Persona_classifier
+import numpy as np
 import torch
 
-# preprocesser
-p = Preprocesser()
-# model
+# models
+USING_TURN = 5
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-chat_model = GPT2LMHeadModel.from_pretrained("./model/hf_form").to(device)
-compress_model = BertModel.from_pretrained(p.COMPRESS_MODEL_NAME, use_cache=True, cache_dir="./model/compress")
+# embedding model
+embedding_model = torch.nn.Embedding(51200, 768).to(device)
+embedding_model.load_state_dict(torch.load("./models/embedding_model/embedding_model_state.pt"))
+# chat_model
+chat_model = GPT2LMHeadModel.from_pretrained("./models/gen_chat_model/trainer").to(device)
+# # persona_classifier
+# persona_classifier = Persona_classifier(Persona_classifier.add_model_argments(parser=None, return_nameSpace=True)).to(device)
+# persona_classifier.load_state_dict(torch.load("./models/persona_classifier/best_model_state.pt", map_location=torch.device(device)))
+# # persona_converter
+# persona_converter = BartForConditionalGeneration.from_pretrained("./models/persona_converter/trainer").to(device)
+# tokenizers
+tokenizer_for_chat = PreTrainedTokenizerFast.from_pretrained("./tokenizer/GPT")
+tokenizer_for_persona = PreTrainedTokenizerFast.from_pretrained("./tokenizer/koBart")
+tokenizer_for_chat.encode("토크나이저 준비")
+tokenizer_for_persona.encode("토크나이저 준비")
+
+# labels
+label_dict = {0: '[HAPPY]', 1: '[PANIC]', 2: '[ANGRY]', 3: '[UNSTABLE]', 4: '[HURT]', 5: '[SAD]', 6: '[NEUTRAL]'}
--- model_use.py
+++ model_use.py
@@ -1,45 +1,73 @@
-from typing import List, Dict
-import numpy as np
-import __init__
-import torch
+from typing import List, Dict, Union
+from HaYan_NLP import *
 
-USING_TURN = 2
-EOS = __init__.p.tokenizer.eos_token
-def chat_with_model(user_text: str, past_convs: List[str]) -> str:
-    user_text = __init__.p.encoding(user_text+EOS)
-    for i in range(len(past_convs)-1, len(past_convs)-(1+USING_TURN*2), -1):
+def chat_with_bot(
+        user_text: str,
+        user_emotion_id: int,
+        past_convs: List[str],
+        DB_QnA: Dict[str, str]) -> str:
+    """
+    chat with bot
+
+    Args:
+        user_text : the user spoken text.
+        user_emotion_id : the Id for emotion of user. following the label_dict.
+        past_convs : the List of past convs. it has to order by time.
+        DB_QnA : the Dict including prepared Questions(Key) and Answers(Value).
+    """
+
+    # searching question in prepared questions
+    searched_answer = searching_QnA(user_text, DB_QnA)
+    if searched_answer is not None:
+        return searched_answer
+    # check the user_input has hate expression
+    pass
+    # tokenizing user_text and past conversations(last 5 turn(10 sent))
+    emotion_token = label_dict[user_emotion_id]
+    user_text = tokenizer_for_chat.encode(emotion_token + user_text, return_tensors="pt").to(device)
+    for i in range(len(past_convs)-1, len(past_convs)-(USING_TURN*2)-1, -1):
         if i < 0:
             break
-        encoded_vector = __init__.p.encoding(past_convs[i]+EOS)
+        encoded_vector = tokenizer_for_chat.encoding(past_convs[i] + tokenizer_for_chat.eos_token)
         user_text = torch.cat([encoded_vector, user_text], dim=-1)
+    # generate bot answer
+    output = chat_model.generate(user_text,
+                                 max_length=1000,
+                                 num_beams=5,
+                                 top_k=20,
+                                 no_repeat_ngram_size=4,
+                                 length_penalty=0.65,
+                                 repetition_penalty=2.)
+    bot_output = tokenizer_for_chat.decode(output[0][user_text.shape[-1]:])
+    # # check the generated answer has correct persona(returned persona percentage of sentence)
+    # if persona_classifier.predict(bot_output) < 0.2:
+    #     # if bot output does not have enough persona, change output using model
+    #     bot_text = tokenizer_for_persona.encode(bot_output, return_tensors="pt").to(device)
+    #     output = persona_converter.generate(bot_text,
+    #                                         max_length=1000,
+    #                                         num_beams=5,
+    #                                         top_k=20,
+    #                                         no_repeat_ngram_size=4,
+    #                                         length_penalty=0.65,
+    #                                         repetition_penalty=2.)
+    #     bot_output = tokenizer_for_persona.decode(output[0][bot_text.shape[-1]:])
+    return bot_output
 
-    user_text = user_text.to(__init__.device)
-    output = __init__.chat_model.generate(user_text,
-                                          max_length=1000,
-                                          num_beams=5,
-                                          top_k=20,
-                                          no_repeat_ngram_size=4,
-                                          length_penalty=0.65,
-                                          repetition_penalty=2.)
-
-    return __init__.p.decoding(output[0][user_text.shape[-1]:])
-
-def searching_QnA(user_text: str, DB_QnA: Dict[str, str]) -> str:
-    max_sim = 0.9
+def searching_QnA(user_text: str, DB_QnA: Dict[str, str]) -> Union[str, None]:
+    min_sim = 0.8
     searched_answer = None
+    user_text = embedding_sent(user_text)
 
-    def compress_sent(sent: str) -> List[float]:
-        encoded_sent = __init__.p.compress_tokenizer.encode(sent, return_tensors="tf")
-        return __init__.compress_model(encoded_sent)["pooler_output"].numpy().tolist()
-
-    def cos_sim(v1: List[float], v2: List[float]) -> np.float:
-        return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
-
-    user_text = compress_sent(user_text)
     for question, answer in DB_QnA.items():
-        temp_sim = cos_sim(user_text, compress_sent(question))
-        if temp_sim > max_sim:
-            max_sim = temp_sim
+        temp_sim = cos_sim(user_text, embedding_sent(question))
+        if temp_sim > min_sim:
+            min_sim = temp_sim
             searched_answer = answer
-
     return searched_answer
+
+def embedding_sent(sent: str) -> List[float]:
+    sent = tokenizer_for_chat.encode(sent, return_tensors="pt")
+    return embedding_model(sent)
+
+def cos_sim(v1: List[float], v2: List[float]) -> np.float:
+    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
